68065b3bd3657cfb6242a1fc6f8da22b08ffa249
vishaljamshedpur19 <162937526+vishaljamshedpur19@users.noreply.github.com>
2024-03-11 07:43:56 +0530

    Create PatienthubApplicationTests.java

---
+++ src/test/java/com/patienthub/patienthub/PatienthubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patienthub.patienthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatienthubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
