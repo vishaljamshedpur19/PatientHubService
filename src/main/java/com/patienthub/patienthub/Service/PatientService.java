5be3cfc023a99a086c02cd5b783472d1c8b15071
vishaljamshedpur19 <162937526+vishaljamshedpur19@users.noreply.github.com>
2024-03-11 10:24:11 +0530

    Update PatientService.java

--- src/main/java/com/patienthub/patienthub/Service/PatientService.java
+++ src/main/java/com/patienthub/patienthub/Service/PatientService.java
@@ -11,13 +11,37 @@ import com.patienthub.patienthub.exceptions.ResourceNotFoundException;
 
 @Service
 public class PatientService {
-	@Autowired
+@Autowired
     private PatientRepo  patientRepository;
 
     @Cacheable("patients")
     public Patient getPatientById(Long id) {
         return patientRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Patient not found with id: " + id));
                      
+    }
+
+
+
+    public java.util.List<Patient> getAllPatients() {
+        return patientRepository.findAll();
+    }
+
+  
+
+    public Patient addPatient(Patient patient) {
+    	
+    	Patient patient2 = new Patient();
+
+        return patientRepository.save(patient);
+    }
+
+    public Patient updatePatient(Long id, Patient patient) {
+        patient.setId(id);
+        return patientRepository.save(patient);
+    }
+
+    public void deletePatient(Long id) {
+        patientRepository.deleteById(id);
     }
 
 }
